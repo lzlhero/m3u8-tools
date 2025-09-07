380b31760b77c3e613c56eab2bd92b5c4b3924c0
lzlhero <lzlhero@gmail.com>
2025-09-07 09:33:26 +0800

    ignore ads url

--- src/urlm3u8.js
+++ src/urlm3u8.js
@@ -20,39 +20,55 @@ const { readFile, writeFile } = require('fs/promises');
 
   // parsing and transforming TS url
   var lines = content.split(/\r?\n/);
-  var urlLines = [], fileLines = [], url;
+  var urlObject, urlSections, urlFilename, urlPathname, referPathname;
+  var urlLines = [], fileLines = [];
   for (var i = 0; i < lines.length; i++) {
     if (lines[i].trim().length > 0 && !lines[i].startsWith('#')) {
-      url = baseUrl ? new URL(lines[i], baseUrl) : new URL(lines[i]);
-      // get absolute url
-      urlLines.push(url.href);
-      // get local filename
-      fileLines.push(url.pathname.split('/').pop());
+      urlObject = baseUrl ? new URL(lines[i], baseUrl) : new URL(lines[i]);
+      urlSections = urlObject.href.split('/');
+      urlFilename = urlSections.pop();
+      urlPathname = urlSections.join('/');
+
+      // ignore TS file which is not in the same path
+      if (!referPathname) {
+        // set the first TS file path to refer path
+        referPathname = urlPathname;
+      }
+
+      // save useful url and filename
+      if (referPathname === urlPathname) {
+        urlLines.push(urlObject.href);
+        fileLines.push(urlFilename);
+      }
     } else {
+      // save comments
       urlLines.push(lines[i]);
       fileLines.push(lines[i]);
     }
   }
 
-  // get key url
-  var tsKeys = {};
-  var fileContent = fileLines.join('\n').replace(/(?:URI=")([^"]+)(?:")/g, function($0, tsKey) {
-    var url = baseUrl ? new URL(tsKey, baseUrl) : new URL(tsKey);
-    tsKeys[url] = null;
-    return `URI="${url.pathname.split('/').pop()}"`;
+  // parsing and transforming KEY url
+  var keyLines = {};
+  var fileContent = fileLines.join('\n').replace(/(?:URI=")([^"]+)(?:")/g, function($0, $1) {
+    var urlObject = baseUrl ? new URL($1, baseUrl) : new URL($1);
+    keyLines[urlObject.href] = null;
+    return `URI="${urlObject.pathname.split('/').pop()}"`;
   });
+  keyLines = Object.keys(keyLines);
 
-  // save key.txt content.
-  var keyFile = 'key.txt';
-  try {
-    await writeFile(keyFile, Object.keys(tsKeys).join('\n'), 'utf8');
-  } catch (error) {
-    console.error(`Write "${keyFile}" failed.`);
-    process.exit(1);
+  // save key.txt content
+  if (keyLines.length) {
+    var keyFile = 'key.txt';
+    try {
+      await writeFile(keyFile, keyLines.join('\n'), 'utf8');
+    } catch (error) {
+      console.error(`Write "${keyFile}" failed.`);
+      process.exit(1);
+    }
+    console.log(`Write "${keyFile}" file.`);
   }
-  console.log(`Write "${keyFile}" file.`);
 
-  // save url.m3u8 content.
+  // save url.m3u8 content
   var urlM3U8 = 'url.m3u8';
   try {
     await writeFile(urlM3U8, urlLines.join('\n'), 'utf8');
@@ -62,7 +78,7 @@ const { readFile, writeFile } = require('fs/promises');
   }
   console.log(`Write "${urlM3U8}" file.`);
 
-  // save file.m3u8 content.
+  // save file.m3u8 content
   var fileM3U8 = 'file.m3u8';
   try {
     await writeFile(fileM3U8, fileContent, 'utf8');

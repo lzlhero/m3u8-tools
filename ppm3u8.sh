309ad5d06ad2dddc730601da7838e59d33fd4b8d
lzlhero <lzlhero@gmail.com>
2025-09-09 13:37:59 +0800

    quotes

--- ppm3u8.sh
+++ ppm3u8.sh
@@ -9,6 +9,6 @@ fi
 node "$(dirname "$0")/src/ppm3u8.js" "$1" "$m3u8_url"
 
 if [ -f "key.txt" ]; then
-  echo "Downloading key from \"key.txt\" file..."
+  echo 'Downloading key from "key.txt" file...'
   wget -q -c -i key.txt
 fi

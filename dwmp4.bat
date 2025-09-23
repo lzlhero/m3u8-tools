2e5e3fe76aa1840d4b3949f63213823fd232c48b
lzlhero <lzlhero@gmail.com>
2025-09-23 22:21:41 +0800

    prompt

--- dwmp4.bat
+++ dwmp4.bat
@@ -38,7 +38,7 @@ call dw -i ts.txt
 
 :: prompt user to stop
 echo.
-echo Merge all ts files to "%output%" file? (Ctrl+C to stop)
+echo Merge all ts files to "%output%" file? Press Ctrl+C to exit.
 pause
 
 :: set m3u8 input filename

563c5c4846d462610ff2c8129e3e17ca86180913
lzlhero <lzlhero@gmail.com>
2025-09-29 20:09:42 +0800

    ppm3u8 tool

--- ppm3u8.bat
+++ ppm3u8.bat
@@ -1,12 +1,6 @@
 @echo off
 
-if exist "%~2" (
-  set /p m3u8-url=<%~2
-) else (
-  set "m3u8-url=%~2"
-)
-
-node "%~dp0\src\ppm3u8.js" "%~1" %m3u8-url%
+node "%~dp0\src\ppm3u8.js" %*
 
 if exist "key.txt" (
   echo Downloading key from "key.txt" file...
